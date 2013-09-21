7e49584d9bafe14af6ad8dc2c0dc070d22ce5b58
Mike Conley <mike.d.conley@gmail.com>
2013-09-21 06:36:56 -0700

    Whoops - forgot the TemplateHook.

--- reviewboard_persona/extension.py
+++ reviewboard_persona/extension.py
@@ -2,7 +2,10 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include
 from reviewboard.extensions.base import Extension
+from reviewboard.extensions.hooks import TemplateHook
 
 class RBPersona(Extension):
     def __init__(self, *args, **kwargs):
         super(RBPersona, self).__init__(*args, **kwargs)
+        self.script_injection = TemplateHook(self, "base-scripts-post",
+            template_name="reviewboard_persona/base.html")
