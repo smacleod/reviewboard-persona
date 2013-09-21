224dd6e452e8b81eaea754ff0a583d49769e5ce5
Steven MacLeod <steven@smacleod.ca>
2013-09-21 11:59:16 -0400

    Add the API resource for verifying assertions
    There is now a Web API resource to verify login assertions with the
    Persona verifier. This resource will not log the user in yet, but will
    verify the assertion and return the proper response.

--- reviewboard_persona/extension.py
+++ reviewboard_persona/extension.py
@@ -1,10 +1,23 @@
 # ReviewBoard-Persona Extension for Review Board.
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include
+
 from reviewboard.extensions.base import Extension
 from reviewboard.extensions.hooks import TemplateHook
 
+from reviewboard_persona.resources import persona_login_resource
+
 class RBPersona(Extension):
+
+    metadata = {
+        'Name': 'Review Board Persona',
+        'Author': 'Steven MacLeod, Mike Conley',
+    }
+
+    resources = [
+        persona_login_resource,
+    ]
+
     def __init__(self, *args, **kwargs):
         super(RBPersona, self).__init__(*args, **kwargs)
         self.script_injection = TemplateHook(self, "base-scripts-post",
---
+++ reviewboard_persona/resources.py
@@ -0,0 +1,82 @@
+import urllib2
+import urllib
+
+from django.contrib.sites.models import Site
+from django.utils import simplejson
+
+from djblets.siteconfig.models import SiteConfiguration
+from djblets.webapi.decorators import webapi_response_errors, \
+                                      webapi_request_fields
+from djblets.webapi.errors import INVALID_FORM_DATA
+
+from reviewboard.webapi.decorators import webapi_check_local_site
+from reviewboard.webapi.resources import WebAPIResource
+
+
+class PersonaLoginResource(WebAPIResource):
+    """Resource for logging in using Mozilla Persona.
+
+    The provided assertion will be verified by contact a remote server.
+    If the assertion is verified, the user should be logged in and a
+    200 response be returned. In the event that the assertion cannot
+    be verified, a 500 will be returned.
+    """
+    name = 'persona_login'
+    allowed_methods = ('GET', 'POST',)
+
+    VERIFICATION_URL = "https://verifier.login.persona.org/verify"
+
+    @webapi_check_local_site
+    @webapi_response_errors(INVALID_FORM_DATA)
+    @webapi_request_fields(
+        required={
+            'assertion': {
+                'type': str,
+                'description': 'The login assertion to be verified.'
+            },
+        },
+    )
+    def create(self, request, assertion, *args, **kwargs):
+        """Attempts to verify an assertion and login the user."""
+        if not assertion:
+            # The assertion was empty
+            return INVALID_FORM_DATA, {
+                'fields': {
+                    'assertion': 'The assertion was empty',
+                },
+            }
+
+        try:
+            response = self.verify_assertion(assertion)
+
+            if  hasattr(response, 'status')  and response.status != "okay":
+                # The assertion could not be verified
+                return 500, {}
+
+            email = response.email
+
+        except Exception, e:
+            # There was a problem verifying the assertion
+            return 500, {}
+
+        return 200, {}
+
+    def get_audience_url(self):
+        """Return the domain and port of this Review Board instance."""
+        protocol = SiteConfiguration.objects.get_current().get("site_domain_method")
+        domain = Site.objects.get_current().domain
+        return '%s://%s' % (protocol, domain)
+
+    def  verify_assertion(self, assertion):
+        """Make an assertion verification request and return the response."""
+        fields = {
+            'assertion': assertion,
+            'audience': self.get_audience_url(),
+        }
+
+        u = urllib2.urlopen(self.VERIFICATION_URL, urllib.urlencode(fields))
+        rsp = u.read()
+
+        return simplejson.loads(rsp)
+
+persona_login_resource = PersonaLoginResource()
